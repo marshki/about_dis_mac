abd59253e4ddafbda727d899f8529f070c91d413
marshki <marshall@morethantomorrow.org>
2017-10-23 17:09:17 -0400

    testing.py

--- testing.py
+++ testing.py
@@ -1,19 +1,25 @@
 #!/bin/env python
 
-import os 
-import sys 
+import os
+import sys
+import pwd
 
 """
 Add local user account in Ubuntu 16.04 
 """
 
-def rootCheck(): 
+def rootCheck():
         """Check is user has UID 0"""
 
         if not os.getuid() == 0:
                 print("This program requires ROOT privileges. Exiting.")
-                sys.exit() 
+                sys.exit()
 
-def usernameCheck(): 
+def userName():
+        """Prompt for username"""
+
+
+def userNameCheck(): 
         """Check if username exists"""
-
+        pwd.getpwnam('fentonadmin')
+        os.system('grep -w "root" /etc/passwd')
