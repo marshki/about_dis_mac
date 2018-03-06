d16b31408bebf96b701d7e6335bb542b1601640f
marshki <marshall@morethantomorrow.org>
2018-03-05 21:36:42 -0500

    test processor function

--- src/processor.sh
+++ src/processor.sh
@@ -1,3 +1,9 @@
-system_profiler SPHardwareDataType \
-  | sed -n -e '/Processor Name/{;s/.*: //;h;}' \
-           -e '/Processor Speed/{;s/.*: //;G;s/\n/ /;p;q;}'
+#!/bin/bash
+# Get CPU Name, Speed
+
+function processor () {
+	local cpu=$(system_profiler SPHardwareDataType| sed -n -e '/Processor Name/{;s/.*: //;h;}' -e '/Processor Speed/{;s/.*: //;G;s/\n/ /;p;q;}')
+	printf "%s\n" "${cpu}"
+}
+
+processor
