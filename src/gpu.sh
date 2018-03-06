67447ab827c7c2ab58333a3b14566165230a80c0
marshki <marshall@morethantomorrow.org>
2018-03-06 11:54:13 -0500

    add gpu function--testing

---
+++ src/gpu.sh
@@ -0,0 +1,8 @@
+#system_profiler SPDisplaysDataType 
+
+system_profiler SPDisplayDataType \
+  | sed -n -e '/Chipset Model/{;s/.*: //;h;}' \
+           -e '/VRAM/{;s/.*: //;G;s/\n/ /;p;q;}'
+
+# |grep --extended-regexp 'Chipset Model: '\|'VRAM \(Dynamic, Max\): '|sed 's/^.*: //'|head -2
+
