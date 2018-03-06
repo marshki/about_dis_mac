e82d0eddbf9a47fcc3b93a719ae5a49e9bff85d4
marshki <marshall@morethantomorrow.org>
2018-03-05 21:41:52 -0500

    improve readability for processor.sh

--- src/processor.sh
+++ src/processor.sh
@@ -2,7 +2,10 @@
 # Get CPU Name, Speed
 
 function processor () {
-	local cpu=$(system_profiler SPHardwareDataType| sed -n -e '/Processor Name/{;s/.*: //;h;}' -e '/Processor Speed/{;s/.*: //;G;s/\n/ /;p;q;}')
+	local cpu=$(system_profiler SPHardwareDataType|\
+	sed -n -e '/Processor Name/{;s/.*: //;h;}'\
+	-e '/Processor Speed/{;s/.*: //;G;s/\n/ /;p;q;}')
+	
 	printf "%s\n" "${cpu}"
 }
 
