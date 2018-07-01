04b918667aef9ddc019f7e3acfd81dc73169c5a2
marshki <marshall@morethantomorrow.org>
2018-07-01 10:17:35 -0400

    add placeholders

---
+++ functions/operating_system.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+# OS version 
+
+operating_system () {
+  local os=$(sw_vers -productVersion)
+
+  #write_header "OS Version"
+  printf "%s\\n" "${os}"
+  #printf "%s\\n" ""
+}
+
+operating_system
