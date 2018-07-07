4c4483e63726e36f05bcb874cfafb2d9a8484d7f
marshki <marshall@morethantomorrow.org>
2018-07-07 10:49:38 -0400

    add processor function

---
+++ functions/processor.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Processor Speed & Name
+
+processor () {
+
+  local cpu=$(system_profiler SPHardwareDataType   | awk '/Processor (Speed|Name):/ { sub(/^.*: /, ""); print; }'| sort)
+
+  #write_header "Processor"
+  printf "%s\\n" "${cpu}"
+  #printf "%s\\n" ""
+} 
+
+processor
