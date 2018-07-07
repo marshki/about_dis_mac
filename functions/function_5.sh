2e54a23def04cf10d0d4f7df9385bb084052b3ff
marshki <marshall@morethantomorrow.org>
2018-07-07 12:16:46 -0400

    awk goodness

--- functions/function_5.sh
+++ functions/function_5.sh
@@ -3,11 +3,11 @@
 ### Retrieve memory information ###
 
 memory () {
+  local ram=$(system_profiler SPHardwareDataType | awk '/Memory/ { sub(/^.*: /, ""); print; }')
+  local type=$(system_profiler SPMemoryDataType |awk '/Type/'|sed 's/^.*: //')
 
-  local ram=$(system_profiler SPHardwareDataType |awk '/Memory/'|sed 's/^.*: //')	
-
-
-  local type=$(system_profiler SPMemoryDataType |grep --extended-regexp 'Type: '\|'Speed: '|sed 's/^.*: //'|head -2)
+  #system_profiler SPHardwareDataType   | awk '/Processor (Speed|Name):/ { sub(/^.*: /, ""); print; }q'
+  #local type=$(system_profiler SPMemoryDataType |grep --extended-regexp 'Type: '\|'Speed: '|sed 's/^.*: //'|head -2)
 
     #write_header "Memory"
     printf "%s\\n" "${ram}"
