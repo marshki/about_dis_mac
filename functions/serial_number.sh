564c1d16ab87177e7bafa23935c6d600403083d1
marshki <marshall@morethantomorrow.org>
2018-07-07 11:02:28 -0400

    comments

--- functions/serial_number.sh
+++ functions/serial_number.sh
@@ -3,8 +3,8 @@
 
 serial_number () {
 
-  local serialnum01=$(system_profiler SPHardwareDataType |awk '/Serial/'|sed 's/^.*: //')
-  local serialnum02=$(system_profiler SPHardwareDataType| awk '/Serial/ {print $4}')
+  local serialnum01=$(system_profiler SPHardwareDataType |awk '/Serial/'|sed 's/^.*: //')  #safer
+  local serialnum02=$(system_profiler SPHardwareDataType| awk '/Serial/ {print $4}')       #cleaner 
 
   #write_header "Serial Number"
   printf "%s\\n" "${serialnum01}"
