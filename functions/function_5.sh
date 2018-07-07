5839a90d96ed73ffc898e7b3de3e9ef5aa1274f9
marshki <marshall@morethantomorrow.org>
2018-07-07 11:20:13 -0400

    adding to develop

--- functions/function_5.sh
+++ functions/function_5.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+### Retrieve memory information ###
+
+memory () {
+
+	local ram=$(system_profiler SPHardwareDataType |grep --extended-regexp 'Memory: ' |sed 's/^.*: //')
+	local type=$(system_profiler SPMemoryDataType |grep --extended-regexp 'Type: '\|'Speed: '|sed 's/^.*: //'|head -2)
+
+	#write_header "Memory"
+	printf "%s\\n" "${ram}"
+	printf "%s\\n" "${type}"
+	#printf "%s\\n" ""
+} 
+
+memory
