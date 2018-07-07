185323db0f295747b24733ab9929ab88baa895a1
marshki <marshki@users.noreply.github.com>
2018-07-07 17:43:50 -0400

    awk rules them all

--- functions/serial_number.sh
+++ functions/serial_number.sh
@@ -2,13 +2,10 @@
 # Serial number 
 
 serial_number () {
-
-  local serialnum01=$(system_profiler SPHardwareDataType |awk '/Serial/'|sed 's/^.*: //')  #safer
-  local serialnum02=$(system_profiler SPHardwareDataType| awk '/Serial/ {print $4}')       #cleaner 
-
+  local serialnum=$(system_profiler SPHardwareDataType | awk '/Serial/ { sub(/^.*: /, ""); print; }')
+  
   #write_header "Serial Number"
-  printf "%s\\n" "${serialnum01}"
-  printf "%s\\n" "${serialnum02}"
+  printf "%s\\n" "${serialnum}"
   #printf "%s\\n" ""
 }
 
