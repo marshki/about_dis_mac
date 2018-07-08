b5809616b26a1b1eb87417118d60925407b285d8
marshki <marshall@morethantomorrow.org>
2018-07-08 12:09:54 -0400

    update comments

--- functions/startup_disk.sh
+++ functions/startup_disk.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+# Can `disk` be done purely `awk`? 
+# Output of both functions on single line? Does 'ADM' show both?  
+ 
 startup_disk () {
   local disk=$(system_profiler SPStorageDataType |awk 'FNR == 3 {print}'|sed 's/[[:blank:]:]*//g')
   local mount=$(system_profiler SPStorageDataType | awk '/Mount Point/ { sub(/^.*: /, ""); print; }')
