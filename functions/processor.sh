42c038f8100d9792e9fa4ead386e6ec98ca17e00
marshki <marshall@morethantomorrow.org>
2018-07-08 08:09:40 -0400

    remove old comments

--- functions/processor.sh
+++ functions/processor.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 # Processor Speed & Name
-# Is there a way to get awk to sort output to a single line? 
 
 processor () {
 
-  local cpu=$(system_profiler SPHardwareDataType |awk '/Processor (Speed|Name):/ { sub(/^.*: /, ""); print; }'|sort |xargs)
-
+  local cpu=$(system_profiler SPHardwareDataType | awk '/Processor (Speed|Name):/ { sub(/^.*: /, ""); print; }'| sort | xargs)
   #write_header "Processor"
   printf "%s\\n" "${cpu}"
   #printf "%s\\n" ""
