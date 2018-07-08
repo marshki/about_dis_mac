b9c27f8e2c75e1fcd04720cdf1733e5f3c902a3a
marshki <marshall@morethantomorrow.org>
2018-07-08 11:11:36 -0400

    comments

--- functions/graphics.sh
+++ functions/graphics.sh
@@ -2,7 +2,8 @@
 #Model and VRAM
 # VRAM(Total):
 # VRAM(Dynamic, Max):
- 
+# regexp should match Model AND either Max OR Total
+  
 graphics () {
 
   local gpu=$(system_profiler SPDisplaysDataType | awk '/(Model|Max\)|Total\)):/ { sub(/^.*: /, ""); print; }'| xargs)
