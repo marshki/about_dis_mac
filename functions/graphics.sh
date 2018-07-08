d6039135205dc95abc19981db6680234e36e80e3
marshki <marshall@morethantomorrow.org>
2018-07-08 11:10:16 -0400

    add expression for vram total,max

--- functions/graphics.sh
+++ functions/graphics.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #Model and VRAM
-
+# VRAM(Total):
+# VRAM(Dynamic, Max):
+ 
 graphics () {
 
-  local gpu=$(system_profiler SPDisplaysDataType | awk '/(Model|Max\)):/ { sub(/^.*: /, ""); print; }'| xargs)
+  local gpu=$(system_profiler SPDisplaysDataType | awk '/(Model|Max\)|Total\)):/ { sub(/^.*: /, ""); print; }'| xargs)
   #write_header "Processor"
   printf "%s\\n" "${gpu}"
   #printf "%s\\n" ""
