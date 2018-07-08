6ceeb5e526691a78788214a4f44c559e8afb3066
marshki <marshall@morethantomorrow.org>
2018-07-08 08:05:09 -0400

    spacing in awk

--- functions/memory.sh
+++ functions/memory.sh
@@ -3,8 +3,8 @@
 ### Retrieve memory information ###
 
 memory () {
-  local ram=$(system_profiler SPHardwareDataType | awk '/Memory/ { sub(/^.*: /, ""); print; }')
-  local type=$(system_profiler SPMemoryDataType |awk '/(Type|Speed):/ { sub(/^.*: /, ""); print; }'|head -2|xargs)
+  local ram=$(system_profiler SPHardwareDataType | awk '/Memory/{ sub(/^.*: /, ""); print; }')
+  local type=$(system_profiler SPMemoryDataType | awk '/(Type|Speed):/ { sub(/^.*: /, ""); print; }'|head -2|xargs)
 
     #write_header "Memory"
     printf "%s\\n" "${ram}"
