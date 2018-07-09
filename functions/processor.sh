375ef7e3fc417f813a9503c7322cae5337cf5daa
marshki <marshall@morethantomorrow.org>
2018-07-09 10:32:27 -0400

    add newline for readability

--- functions/processor.sh
+++ functions/processor.sh
@@ -3,10 +3,9 @@
 
 processor () {
 
-  local cpu=$(system_profiler SPHardwareDataType | awk '/Processor (Speed|Name):/ { sub(/^.*: /, ""); print; }'| sort | xargs)
-  #write_header "Processor"
+  local cpu=$(system_profiler SPHardwareDataType | awk '/Processor (Speed|Name):/ { sub(/^.*: /, ""); print; }'\
+  | sort | xargs)
   printf "%s\\n" "${cpu}"
-  #printf "%s\\n" ""
 } 
 
 processor
