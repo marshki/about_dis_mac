f2db17e4645e3c676fbfc3169af6614eb7ee81c4
marshki <marshall@morethantomorrow.org>
2018-07-09 10:30:18 -0400

    update comments

--- functions/memory.sh
+++ functions/memory.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-### Retrieve memory information ###
+# Retrieve memory information 
 # How can we print the output of both commands to a single line? 
 
 memory () {
   local ram=$(system_profiler SPHardwareDataType | awk '/Memory/{ sub(/^.*: /, ""); print; }')
   local type=$(system_profiler SPMemoryDataType | awk '/(Type|Speed):/ { sub(/^.*: /, ""); print; }'|head -2|xargs)
   
-  #write_header "Memory"
   printf "%s\\n" "${ram}"
   printf "%s\\n" "${type}"
-  #printf "%s\\n" ""
 } 
 
 memory
