9735c1c5e5043d5e7a7a01693204ab8cae3b7a19
marshki <marshall@morethantomorrow.org>
2018-07-09 10:36:21 -0400

    update comments

--- functions/startup_disk.sh
+++ functions/startup_disk.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 # Can `disk` be done purely `awk`? 
-# Output of both functions on single line? Does 'ADM' show both?  
+# Output of both functions on single line? Does 'ATM' show both? Do we need `Mount Point`?   
  
 startup_disk () {
   local disk=$(system_profiler SPStorageDataType |awk 'FNR == 3 {print}'|sed 's/[[:blank:]:]*//g')
   local mount=$(system_profiler SPStorageDataType | awk '/Mount Point/ { sub(/^.*: /, ""); print; }')
 
-  #write_header "Startup Disk"
   printf "%s\\n" "${disk}"
   printf "%s\\n" "${mount}"
-  #printf "%s\\n" ""
 }
 
 startup_disk
