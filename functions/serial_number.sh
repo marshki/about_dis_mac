8888b4eaa659b51c56e15c57158d178085189666
marshki <marshall@morethantomorrow.org>
2018-07-09 10:34:49 -0400

    remove superfluous comments

--- functions/serial_number.sh
+++ functions/serial_number.sh
@@ -4,9 +4,7 @@
 serial_number () {
   local serialnum=$(system_profiler SPHardwareDataType | awk '/Serial/ { sub(/^.*: /, ""); print; }')
   
-  #write_header "Serial Number"
   printf "%s\\n" "${serialnum}"
-  #printf "%s\\n" ""
 }
 
 serial_number
