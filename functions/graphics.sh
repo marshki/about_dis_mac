bc711243c86849c1bc8cc23abc1d55b33b702941
marshki <marshall@morethantomorrow.org>
2018-07-09 10:14:01 -0400

    clarify comments; add follow-up

--- functions/graphics.sh
+++ functions/graphics.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-#Model and VRAM
-# VRAM(Total):
-# VRAM(Dynamic, Max):
-# regexp should match Model AND either Max OR Total
+# Model and VRAM
+# In OS X 10.10, 10.11 --> VRAM(Total):
+# In OS X 10.12, 10.13 --> VRAM(Dynamic, Max):
+# regexp for `awk` should match Model AND either Max OR Total
   
 graphics () {
 
   local gpu=$(system_profiler SPDisplaysDataType | awk '/(Model|Max\)|Total\)):/ { sub(/^.*: /, ""); print; }'| xargs)
-  #write_header "Processor"
   printf "%s\\n" "${gpu}"
-  #printf "%s\\n" ""
 } 
 
 graphics
