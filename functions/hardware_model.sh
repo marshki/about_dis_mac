d14e393267c7179d1faf0ca72c15d08df106f7ad
marshki <marshall@morethantomorrow.org>
2018-07-09 10:16:04 -0400

    update comments

--- functions/hardware_model.sh
+++ functions/hardware_model.sh
@@ -9,9 +9,7 @@ hardware_model () {
   defaults read ~/Library/Preferences/com.apple.SystemProfiler.plist \
   'CPU Names' | cut -sd '"' -f 4 | uniq)
 
-  #write_header "Hardware Model"
   printf "%s\\n" "${hardware_mod}"
-  #printf "%s\\n" ""
 }
 
 hardware_model
