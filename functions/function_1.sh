9b00704f93a8f026853e29a4bd6d5e4f1bb5c76e
marshki <marshall@morethantomorrow.org>
2018-07-16 12:02:11 -0400

    associative array

--- functions/function_1.sh
+++ functions/function_1.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+# Lookup table for OS X marketing names 
+
+-a OSX_MARKETING=(
+["10"]="Yosemite"
+["11"]="El Capitan"
+["12"]="Sierra"
+["13"]="High Sierra"
+)
+
+
+operating_system () {
+
+  local os=$(sw_vers -productVersion)
+
+  write_header "OS Version" "$os"
+}
+
+if "10" in OSX_MARKETING
