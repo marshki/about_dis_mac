69b8f3fe934c61a4673b9cfc84f88b5b95d9dcf6
marshki <marshall@morethantomorrow.org>
2018-07-16 12:28:43 -0400

    associative array

--- functions/function_1.sh
+++ functions/function_1.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 # Lookup table for OS X marketing names 
 
--a OSX_MARKETING=(
+OSX_MARKETING=(
 ["10"]="Yosemite"
 ["11"]="El Capitan"
 ["12"]="Sierra"
 ["13"]="High Sierra"
 )
 
+[[ -n "${OSX_MARKETING[10]}" ]] && printf '%s is in array\n' "10"
 
-operating_system () {
+#printf "%s\\n" ${OSX_MARKETING[*]}
 
-  local os=$(sw_vers -productVersion)
-
-  write_header "OS Version" "$os"
-}
-
-if "10" in OSX_MARKETING
