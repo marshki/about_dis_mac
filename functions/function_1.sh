ce71407363e949a61b5c94a309c7730636d99a5a
marshki <marshall@morethantomorrow.org>
2018-07-21 11:45:32 -0400

    testing

--- functions/function_1.sh
+++ functions/function_1.sh
@@ -8,13 +8,10 @@ OSX_MARKETING=(
 ["13"]="High Sierra"
 )
 
-[[ -n "${OSX_MARKETING[10]}" ]] && printf '%s is in array\n' "10"
+# Take the number extracted from here & use it as a reference,
+# e.g. "12"
+sw_vers -productVersion| awk -F '[.]' '{print $2}'
 
-
-
+[[ -n "${OSX_MARKETING[12]}" ]] && printf "%s\\n" "${OSX_MARKETING[12]}"
 
 #printf "%s\\n" ${OSX_MARKETING[*]}
-
-# Take the number extracted from here & use it as a reference:
-# sw_vers -productVersion| awk -F '[.]' '{print $2}'
-# e.g. '12'
