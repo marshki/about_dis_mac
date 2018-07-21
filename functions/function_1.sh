239cd2c8b72fa4b7f6760b08e6f5745cf6c14144
marshki <marshall@morethantomorrow.org>
2018-07-21 13:03:57 -0400

    tested on OS X 10.1[0..3]

--- functions/function_1.sh
+++ functions/function_1.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# Take the number extracted from here; use it as a reference
+
+osx_num=$(sw_vers -productVersion| awk -F '[.]' '{print $2}')
+
 # Lookup table for OS X marketing names 
 
 OSX_MARKETING=(
@@ -8,16 +13,9 @@ OSX_MARKETING=(
 ["13"]="High Sierra"
 )
 
-# Take the number extracted from here & use it as a reference,
-# e.g. "12"
-osx_num=$(sw_vers -productVersion| awk -F '[.]' '{print $2}')
-
-#printf "%s\\n" "$osx_num"
-
-# Check if the number extracted is in array (not there yet)
-
+# Check if the number extracted is in array; 
+# if it is print marketing name
+ 
 if [[ -n "${OSX_MARKETING[$osx_num]}" ]]; then 
   printf "%s\\n" "${OSX_MARKETING[$osx_num]}"
 fi
-
-#[[ -n "${OSX_MARKETING[12]}" ]] && printf "%s\\n" "${OSX_MARKETING[12]}"
