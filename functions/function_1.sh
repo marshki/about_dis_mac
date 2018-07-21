1071cbfc5f0ec7815a6e7936d7ad341f4ee46298
marshki <marshall@morethantomorrow.org>
2018-07-21 10:53:19 -0400

    extract string between "." and "."

--- functions/function_1.sh
+++ functions/function_1.sh
@@ -10,5 +10,11 @@ OSX_MARKETING=(
 
 [[ -n "${OSX_MARKETING[10]}" ]] && printf '%s is in array\n' "10"
 
+
+
+
 #printf "%s\\n" ${OSX_MARKETING[*]}
 
+# Take the number extracted from here & use it as a reference:
+# sw_vers -productVersion| awk -F '[.]' '{print $2}'
+# e.g. '12'
