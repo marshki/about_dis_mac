65479e918bce36f888f21777a75ccfb0108dca6f
marshki <marshall@morethantomorrow.org>
2018-07-21 22:29:16 -0400

    rename function1 to osx_name.sh

---
+++ functions/osx_name.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Retrieve Apple's marketing name for installed operating system 
+
+# Take the number extracted from here; use it as a reference
+
+osx_num=$(sw_vers -productVersion| awk -F '[.]' '{print $2}')
+
+# Lookup table for OS X marketing names 
+
+OSX_MARKETING=(
+["10"]="Yosemite"
+["11"]="El Capitan"
+["12"]="Sierra"
+["13"]="High Sierra"
+)
+
+# Check if the number extracted is in array; 
+# if it is print marketing name
+ 
+osx_name () {
+  if [[ -n "${OSX_MARKETING[$osx_num]}" ]]; then 
+    printf "%s\\n" "${OSX_MARKETING[$osx_num]}"
+fi
+}
+
+osx_name
