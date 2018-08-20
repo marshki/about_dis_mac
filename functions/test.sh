6d8b6798d4aa908e8af69613774fd2ecfe364ea6
marshki <marshall@morethantomorrow.org>
2018-08-20 14:54:08 -0400

    testing fix to: The domain/default pair of (/var/root/Library/Preferences/com.apple.SystemProfiler.plist, CPU Names) does not exist . "

---
+++ functions/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Hardware model
+# On some test installs OS X didn't have "com.apple.SystemProfiler.plist" -->
+# What generates this preference list? 
+
+
+hardware_model () {
+
+  local hardware_mod
+  hardware_mod=$(defaults read /Users/$LOGNAME/Library/Preferences/com.apple.SystemProfiler.plist \
+  'CPU Names' | cut -sd '"' -f 4 | uniq)
+
+  printf "%s\\n" "${hardware_mod}"
+}
+
+hardware_model
