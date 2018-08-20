936ad6f6a8ff9e38fc5b5a13d00fbde651c6e6db
marshki <marshall@morethantomorrow.org>
2018-08-20 11:10:02 -0400

    declare/assign local var separately

--- functions/memory.sh
+++ functions/memory.sh
@@ -4,8 +4,11 @@
 # How can we print the output of both commands to a single line? 
 
 memory () {
-  local ram=$(system_profiler SPHardwareDataType | awk '/Memory/{ sub(/^.*: /, ""); print; }')
-  local type=$(system_profiler SPMemoryDataType | awk '/(Type|Speed):/ { sub(/^.*: /, ""); print; }'|head -2|xargs)
+  local ram 
+  ram=$(system_profiler SPHardwareDataType | awk '/Memory/{ sub(/^.*: /, ""); print; }')
+  
+  local type 
+  type=$(system_profiler SPMemoryDataType | awk '/(Type|Speed):/ { sub(/^.*: /, ""); print; }'|head -2|xargs)
   
   printf "%s\\n" "${ram}"
   printf "%s\\n" "${type}"
