3c38eb02dce78033aa58cd2a31b820de237ed79a
marshki <marshall@morethantomorrow.org>
2018-08-20 11:05:31 -0400

    declare/assign local var separately

--- functions/processor.sh
+++ functions/processor.sh
@@ -2,9 +2,10 @@
 # Processor Speed & Name
 
 processor () {
-
-  local cpu=$(system_profiler SPHardwareDataType | awk '/Processor (Speed|Name):/ { sub(/^.*: /, ""); print; }'\
+  local cpu 
+  cpu=$(system_profiler SPHardwareDataType | awk '/Processor (Speed|Name):/ { sub(/^.*: /, ""); print; }'\
   | sort | xargs)
+
   printf "%s\\n" "${cpu}"
 } 
 
