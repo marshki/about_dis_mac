8580bb7fdf44f9a1709d8ee0d84106eed89b0a63
marshki <marshall@morethantomorrow.org>
2018-08-22 12:30:46 -0400

    update: 1 path for root, another for all others?

--- functions/hardware_model.sh
+++ functions/hardware_model.sh
@@ -2,11 +2,12 @@
 # Hardware model
 # On some test installs OS X didn't have "com.apple.SystemProfiler.plist" -->
 # What generates this preference list? 
+# --> root user hardware_mod=$(defaults read ~/Library/Preferences/com.apple.SystemProfiler.plist \
 
 hardware_model () {
 
   local hardware_mod
-  hardware_mod=$(defaults read ~/Library/Preferences/com.apple.SystemProfiler.plist \
+  hardware_mod=$(defaults read /Users/$LOGNAME/Library/Preferences/com.apple.SystemProfiler.plist \
   'CPU Names' | cut -sd '"' -f 4 | uniq)
 
   printf "%s\\n" "${hardware_mod}"
