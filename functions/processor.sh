0fdebcbac3f76c1cf200731bbc8130e416cf5a8f
marshki <marshall@morethantomorrow.org>
2019-05-10 08:22:37 -0400

    refresh comments

--- functions/processor.sh
+++ functions/processor.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
-# Processor Speed & Name
+# Retrieve processor information 
+# awk to extract Processor{Name,Speed}
+# sort so numeric comes first
+# xargs to print to single line
+
+awk_memory () { 
+
+local cpu 
+cpu=$(
+
+awk '
+  $1~/Size/ && $2!~/Empty/ {size+=$2}
+  $1~/Speed/ && $2!~/Empty/ {speed=$2" "$3}
+  $1~/Type/ && $2!~/Empty/ {type=$2}
+  END {print size " GB " speed " " type}
+  ' <<< "$(system_profiler SPHardwareDataType; system_profiler SPMemoryDataType)"
+)
+
+printf "%s\\n" "${ram}"
+}
+
+awk_memory
+
+
+
+
+
+
+
 
 processor () {
   local cpu 
