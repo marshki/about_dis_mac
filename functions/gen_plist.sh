fcf6ef4f2fd7488b4b82d8a8166a17a958140a68
marshki <marshall@morethantomorrow.org>
2019-05-10 08:07:44 -0400

    placeholder--need to investigate

---
+++ functions/gen_plist.sh
@@ -0,0 +1,3 @@
+# On some test installs OS X didn't have "com.apple.SystemProfiler.plist" -->
+# What generates this preference list?
+# --> root user hardware_mod=$(defaults read ~/Library/Preferences/com.apple.SystemProfiler.plist \
