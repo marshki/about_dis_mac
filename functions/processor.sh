11aa349e90e52eca31e089b4abaa2b2e4be65297
marshki <marshall@morethantomorrow.org>
2019-05-10 08:57:11 -0400

    fix

--- functions/processor.sh
+++ functions/processor.sh
@@ -4,28 +4,19 @@
 # sort so numeric comes first
 # xargs to print to single line
 
-awk_memory () { 
+processor () {
 
-local cpu 
-cpu=$(
+  local cpu  
+  cpu=$(system_profiler SPHardwareDataType \
+  | awk '/Processor (Name|Speed):/ { sub(/^.*: /, ""); print; }'\
+  | sort \
+  | xargs)
 
-awk '
-  $1~/Size/ && $2!~/Empty/ {size+=$2}
-  $1~/Speed/ && $2!~/Empty/ {speed=$2" "$3}
-  $1~/Type/ && $2!~/Empty/ {type=$2}
-  END {print size " GB " speed " " type}
-  ' <<< "$(system_profiler SPHardwareDataType; system_profiler SPMemoryDataType)"
-)
-
-printf "%s\\n" "${ram}"
 }
 
-awk_memory
+printf "%s\\n" "${cpu}"
 
-
-
-
-
+processor
 
 
 
