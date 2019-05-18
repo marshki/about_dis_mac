35a150767d2952cdda960bf7cd862b04f7b32af6
marshki <marshall@morethantomorrow.org>
2019-05-18 10:05:34 -0400

    update (new and improved!)

--- src/test.sh
+++ src/test.sh
@@ -2,10 +2,22 @@
 #Get the marketing model name from the plist!
 #Bash script written quickly and poorly by https://reddit.com/u/Thanksagainforlunch
 
-uglymodel="$(system_profiler SPHardwareDataType \ |awk '/Identifier/{ sub(/^.*: /, ""); print; }')"
+device_id () {
 
-/usr/libexec/PlistBuddy -c "Print $uglymodel" /System/Library/PrivateFrameworks/ServerInformation.framework/Versions/A/Resources/English.lproj/SIMachineAttributes.plist |awk '/marketingModel/{ sub(/^.*= /, ""); print; }' 
+  d_id="$(system_profiler SPHardwareDataType \ |awk '/Identifier/{ sub(/^.*: /, ""); print; }')"
+  printf "%s\n" "$d_id" 
+} 
 
+hardware_model () { 
 
+  hardware_mod="$(/usr/libexec/PlistBuddy -c "Print $d_id" /System/Library/PrivateFrameworks/ServerInformation.framework/Versions/A/Resources/English.lproj/SIMachineAttributes.plist |awk '/marketingModel/{ sub(/^.*= /, ""); print; }')" 
+   
+  printf "%s\n" "$hardware_mod" 
+} 
+
+device_id 
+hardware_model
+
+#hardware_model 
 #uglymodel="$(system_profiler SPHardwareDataType | grep Identifier | awk -F: '{print $2}' | tr -d '[:space:]')"
 #/usr/libexec/PlistBuddy -c "Print $uglymodel" /System/Library/PrivateFrameworks/ServerInformation.framework/Versions/A/Resources/English.lproj/SIMachineAttributes.plist | awk -F= '/marketingModel/ {print $2}' | awk '{gsub(/^ +| +$/,"")} {print $0}'
