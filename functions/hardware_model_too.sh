54978071781915cd36898aa78e4c5963f0af5993
marshki <marshall@morethantomorrow.org>
2019-05-19 10:23:28 -0400

    update comments

--- functions/hardware_model_too.sh
+++ functions/hardware_model_too.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-
 # Hardware model, too. 
+# Adapted from this thread: 
+# https://www.reddit.com/r/macsysadmin/comments/bmycmz/submission_about_this_mac_command_line_edition/ 
+# Shout-out to: https://reddit.com/u/Thanksagainforlunch
 
 # Step 1: Get Apple Device ID, e.g. MacBookAir6,2
 # system_profiler to extract SPHardwareDataType,
 # then awk to get Identifier (stipping content to the left of the ":"
 # Step 2: use PlistBuddy to search for device_id
 # then awk to extract marketingModel (stripping content to the left of "="
-
-device_id () {
-
-  d_id="$(system_profiler SPHardwareDataType \ |awk '/Identifier/{ sub(/^.*: /, ""); print; }')"
-  printf "%s\n" "$d_id" 
-} 
 
 hardware_model () { 
   
@@ -25,8 +21,6 @@ hardware_model () {
   printf "%s\n" "$hardware_mod" 
 } 
 
-#device_id 
 hardware_model
 
-#Get the marketing model name from the plist!
 #Bash script written quickly and poorly by https://reddit.com/u/Thanksagainforlunch
