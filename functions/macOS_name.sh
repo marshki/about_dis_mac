c5007d75fe9c824df0681f9ee92617634b9ccc21
marshki <marshall@morethantomorrow.org>
2020-05-20 12:59:06 -0400

    add

---
+++ functions/macOS_name.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+macOS_number=$(sw_vers -productVersion| awk -F '[.]' '{print $2}')
+
+MACOS_MARKETING_NAME=(
+["10"]="Yosemite"
+["11"]="El Capitan"
+["12"]="Sierra"
+["13"]="High Sierra"
+["14"]="Mojave"
+["15"]="Catalina"
+)
+
+macOS_name () {
+  if [[ -n "${MACOS_MARKETING_NAME[$macOS_number]}" ]]; then
+    printf "%s\\n" "${MACOS_MARKETING_NAME[$macOS_number]}"
+fi
+}
+
+macOS_name
+
