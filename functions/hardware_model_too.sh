33b567366cd3e2646e5f771219569e4ac954fc90
marshki <marshall@morethantomorrow.org>
2020-06-05 21:39:28 -0400

    fix typo, add comments

--- functions/hardware_model_too.sh
+++ functions/hardware_model_too.sh
@@ -10,12 +10,35 @@
 # Step 2: use PlistBuddy to search for device_id
 # then awk to extract marketingModel (stripping content to the left of "="
 
+
+# This works ---> 
+
+#hardware_model () { 
+  
+#  device_id="$(system_profiler SPHardwareDataType \ |awk '/Identifier/{ sub(/^.*: /, ""); print; }')"
+
+#  hardware_mod="$(/usr/libexec/PlistBuddy -c "Print $device_id" \
+#  /System/Library/PrivateFrameworks/ServerInformation.framework/Versions/A/Resources/English.lproj/SIMachineAttributes.plist \
+#  |awk '/marketingModel/{ sub(/^.*= /, ""); print; }')" 
+   
+#  printf "%s\n" "$hardware_mod" 
+#} 
+
+
+### Testing this ---> 
+### For example: 
+#shopt -s extglob
+#printf "%s\\n" @(English|en).lproj
+
+
 hardware_model () { 
+
+  shopt -s extglob
   
   device_id="$(system_profiler SPHardwareDataType \ |awk '/Identifier/{ sub(/^.*: /, ""); print; }')"
 
   hardware_mod="$(/usr/libexec/PlistBuddy -c "Print $device_id" \
-  /System/Library/PrivateFrameworks/ServerInformation.framework/Versions/A/Resources/English.lproj/SIMachineAttributes.plist \
+  /System/Library/PrivateFrameworks/ServerInformation.framework/Versions/A/Resources/@(English|en).lproj/SIMachineAttributes.plist \
   |awk '/marketingModel/{ sub(/^.*= /, ""); print; }')" 
    
   printf "%s\n" "$hardware_mod" 
