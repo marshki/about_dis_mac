130aa45e50acb1e49fffb6296662c3a0f6ea5f11
marshki <marshall@morethantomorrow.org>
2020-06-13 18:53:22 -0400

    update a cleaner plist parser; need to comment, and work on wrap

--- functions/hardware_model_too.sh
+++ functions/hardware_model_too.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Hardware model, too. 
 # Adapted from this thread: 
 # https://www.reddit.com/r/macsysadmin/comments/bmycmz/submission_about_this_mac_command_line_edition/ 
@@ -10,38 +10,24 @@
 # Step 2: use PlistBuddy to search for device_id
 # then awk to extract marketingModel (stripping content to the left of "="
 
-
-# This works ---> 
-
-#hardware_model () { 
-  
-#  device_id="$(system_profiler SPHardwareDataType \ |awk '/Identifier/{ sub(/^.*: /, ""); print; }')"
-
-#  hardware_mod="$(/usr/libexec/PlistBuddy -c "Print $device_id" \
-#  /System/Library/PrivateFrameworks/ServerInformation.framework/Versions/A/Resources/English.lproj/SIMachineAttributes.plist \
-#  |awk '/marketingModel/{ sub(/^.*= /, ""); print; }')" 
-   
-#  printf "%s\n" "$hardware_mod" 
-#} 
-
-
-### Testing this ---> 
-### For example: 
 #shopt -s extglob
 #printf "%s\\n" @(English|en).lproj
 
 
 hardware_model () { 
 
-  shopt -s extglob
   
-  device_id="$(system_profiler SPHardwareDataType \ |awk '/Identifier/{ sub(/^.*: /, ""); print; }')"
+  #device_id="$(system_profiler SPHardwareDataType \ |awk '/Identifier/{ sub(/^.*: /, ""); print; }')"
+  
+  shopt -s extglob
 
-  hardware_mod="$(/usr/libexec/PlistBuddy -c "Print $device_id" \
-  /System/Library/PrivateFrameworks/ServerInformation.framework/Versions/A/Resources/@(English|en).lproj/SIMachineAttributes.plist \
-  |awk '/marketingModel/{ sub(/^.*= /, ""); print; }')" 
+  hardware_model=$(/usr/libexec/PlistBuddy -c "print :$(sysctl -n hw.model):_LOCALIZABLE_:marketingModel" /System/Library/PrivateFrameworks/ServerInformation.framework/Versions/A/Resources/@(English|en).lproj/SIMachineAttributes.plist)
+
+  #hardware_mod="$(/usr/libexec/PlistBuddy -c "Print $device_id" \
+  #/System/Library/PrivateFrameworks/ServerInformation.framework/Versions/A/Resources/@(English|en).lproj/SIMachineAttributes.plist \
+  #|awk '/marketingModel/{ sub(/^.*= /, ""); print; }')" 
    
-  printf "%s\n" "$hardware_mod" 
+  printf "%s\n" "$hardware_model" 
 } 
 
 hardware_model
