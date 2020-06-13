4aca502b4ef40003361865c8bb636fb37acb4898
marshki <marshall@morethantomorrow.org>
2020-06-13 06:58:48 -0400

    update comments; still no trigger

---
+++ functions/generate_plist.sh
@@ -0,0 +1,7 @@
+# --> NOTE TO SELF <-- 
+# This preference list ('plist'): 
+# "com.apple.SystemProfiler.plist" 
+# is parsed by the "hardware_model" function. 
+# To trigger this list, one needs to click on the "About This Mac" GUI in macOS. 
+# 
+# hardware_mod=$(defaults read ~/Library/Preferences/com.apple.SystemProfiler.plist \)
