b386c2cee9c7becc9d0de4194c9b3895e7aa6cc5
marshki <marshall@morethantomorrow.org>
2020-06-13 18:20:47 -0400

    comments on TODO

--- functions/hardware_model.sh
+++ functions/hardware_model.sh
@@ -1,12 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash 
 # Hardware model
 # Read preference list, extracting 'CPU Names'
 # cut suppress lines w/out delimiter characters, use '"' as delimiter
 # print only unique string (no dupes)
 
+# `awk` should be able to do this more cleanly
+# so can `sed`:
+# sed 's/^.*=\s"(.*)".*/\1/'
+
 hardware_model () {
 
   local hardware_mod
+
   hardware_mod=$(defaults read /Users/$LOGNAME/Library/Preferences/com.apple.SystemProfiler.plist \
   'CPU Names' | cut -sd '"' -f 4 | uniq)
 
