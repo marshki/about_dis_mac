6c6a6f1f8f79deb7372461e0a3620b5f44074f0f
marshki <marshall@morethantomorrow.org>
2020-06-13 08:20:39 -0400

    rm in favor of more explicit file name

--- functions/gen_plist.sh
+++
@@ -1,3 +0,0 @@
-# On some test installs OS X didn't have "com.apple.SystemProfiler.plist" -->
-# What generates this preference list?
-# --> root user hardware_mod=$(defaults read ~/Library/Preferences/com.apple.SystemProfiler.plist \
