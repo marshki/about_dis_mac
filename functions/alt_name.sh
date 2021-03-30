d527b797130e81e9bf0142ffbac5815ffbca8c00
marshki <marshall@morethantomorrow.org>
2021-03-30 14:10:57 -0400

    update

---
+++ functions/alt_name.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Extract macOS software version number 
+# then lookup number in array and display marketing number.
+
+macOS_number=$(sw_vers -productVersion | awk -F '[.]' '{print $1, $2}')
+printf "%s\\n" "$macOS_number"
+
+
+
+#MACOS_MARKETING_NAME=(
+#["10"]="Yosemite"
+#["11"]="El Capitan"
+#["12"]="Sierra"
+#["13"]="High Sierra"
+#["14"]="Mojave"
+#["15"]="Catalina"
+#["11 2"]="Big Sur"
+#)
+
+macOS_name () {
+
+  if [[ -n "${MACOS_MARKETING_NAME[$macOS_number]}" ]]; then
+    printf "%s\\n" "${MACOS_MARKETING_NAME[$macOS_number]}"
+fi
+}
+
+#macOS_name
