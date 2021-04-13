ec4da0346d2defcb77a1fcd1165a676cc0c83fda
marshki <marshall@morethantomorrow.org>
2021-04-13 11:48:28 -0400

    OS X to masOS

--- functions/graphics.sh
+++ functions/graphics.sh
@@ -4,8 +4,8 @@
 # awk to extract 'Model', 'Max', 'Total'
 # xargs to print output to single line
 
-# In OS X 10.10, 10.11 --> VRAM(Total):
-# In OS X 10.12, 10.13 --> VRAM(Dynamic, Max):
+# In macOS 10.10, 10.11 --> VRAM(Total):
+# In macOS 10.12, 10.13 --> VRAM(Dynamic, Max):
 # regexp for `awk` should match Model AND either Max OR Total
   
 graphics () {
