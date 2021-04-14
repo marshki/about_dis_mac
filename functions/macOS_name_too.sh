700d46b5eddedb6b441eb510fc26abacf41daee5
marshki <marshall@morethantomorrow.org>
2021-04-14 12:20:33 -0400

    older version

--- functions/macOS_name_too.sh
+++ functions/macOS_name_too.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-# Production version
 
-# Post-macOS 11.x.x.
-# Extract fields 1 and 2 from macOS software version number,
-# assign those fields to variables, 
-# run a number check, and display corresponding marketing name.
+# Pre macOS 11.x.x, this was the go-to function.
 
-# Lookup table
+# Extract macOS software version number
+# then lookup number in array and display marketing number.
+
+macOS_number=$(sw_vers -productVersion| awk -F '[.]' '{print $2}')
 
 MACOS_MARKETING_NAME=(
 ["10"]="Yosemite"
@@ -15,22 +14,7 @@ MACOS_MARKETING_NAME=(
 ["13"]="High Sierra"
 ["14"]="Mojave"
 ["15"]="Catalina"
-["16"]="Big Sur"
 )
-
-parser() { 
-
-  read -r field_1 field_2 <<<"$(sw_vers -productVersion | awk -F '[.]' '{ print $1, $2 }')"
-}
-
-macOS_number () {
-
-  if [[ "$field_1" -gt 10 ]]; then 
-    macOS_number=$((field_1 + 5))
-  else
-    macOS_number=$((field_2))
-  fi 
-}
 
 macOS_name () {
 
@@ -39,10 +23,4 @@ macOS_name () {
 fi
 }
 
-main () {
-parser
-macOS_number
 macOS_name
-}
-
-main "$@" 
