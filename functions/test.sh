f4c2b4af89f3f96959cb96cbe8123edfd156b3ba
marshki <marshall@morethantomorrow.org>
2022-07-17 13:19:10 -0400

    simplified way to parse macOS version number

---
+++ functions/test.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+# Production version
+
+# Post-macOS 11.x.x.
+# Extract fields 1 from macOS software version number,
+# assign fields to variable,
+# run a number check, and display corresponding marketing name.
+
+# Lookup table
+
+MACOS_MARKETING_NAME=(
+
+["11"]="Big Sur"
+["12"]="Monterey"
+["13"}="Ventura"
+)
+
+# Display header message.
+
+write_header() {
+
+  local name=$1; shift;
+
+  printf "%s\\n""--------------------\\n$name%s\\n--------------------\\n"
+  printf "%s\\n" "$@"
+}
+
+# Parse assign to eponymous variables.
+
+parser() {
+
+ parser=$(sw_vers -productVersion |awk -F '.' '{ print $1 }')
+
+} 
+
+# If macOS_number in array, assign macOS_name to corresponding marketing name.
+
+macOS_name () {
+
+  if [[ -n "${MACOS_MARKETING_NAME[$parser]}" ]]; then
+    macOS_name=${MACOS_MARKETING_NAME[$parser]}
+fi
+    write_header "macOS" "$macOS_name"
+}
+
+# Wrapper.
+
+main () {
+  parser
+  macOS_name
+}
+
+main "$@"
