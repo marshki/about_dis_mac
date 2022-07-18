adf7ca777ffb883b6b823f2d3df3690e43ed6d76
marshki <marshall@morethantomorrow.org>
2022-07-18 12:35:56 -0400

    simplified parser

---
+++ functions/M1/serial_number.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Retrieve serial number
+# Pipe info from system_profiler
+# awk to extract `Serial` row, then field 4.
+
+serial_number () {
+
+local serialnum
+
+serialnum=$(
+
+awk '
+  $1~/Serial/ {serial=$4}
+  END {print serial}
+  ' <<< "$(system_profiler SPHardwareDataType)"
+)
+
+printf "%s\\n" "${serialnum}"
+}
+
+serial_number
+
+# Simplify: 
+# system_profiler SPHardwareDataType | awk '/Serial/ {print $4}'
