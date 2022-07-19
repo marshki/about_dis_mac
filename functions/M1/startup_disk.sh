eaa7572aadd6f958b35ddeaf95d09db753a9c446
marshki <marshall@morethantomorrow.org>
2022-07-19 13:00:44 -0400

    add

---
+++ functions/M1/startup_disk.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Retrieve startup disk information
+# awk to extract third field
+# sed to print string to the right of ':'
+# awk to extract 'Mount Point'
+# head to get primary drive
+
+startup_disk () {
+  local disk  
+
+  disk=$(system_profiler SPStorageDataType |awk 'FNR == 3 {print}'|sed 's/[[:blank:]:]*//g')
+  
+  printf "%s\\n" "${disk}"
+}
+
+startup_disk
