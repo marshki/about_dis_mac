2fcbc536adc214232892b688d7525f366c4a6468
marshki <marshall@morethantomorrow.org>
2022-07-21 14:58:54 -0400

    revert changes; this one works

--- functions/M1/memory.sh
+++ functions/M1/memory.sh
@@ -2,27 +2,10 @@
 
 # Note: Intel, M1, post-macOS 11.x.x.
 
-# Testing!!!
-
 # RAM
 # Extract size, speed, and type from SPmemoryDataType output
 # Function expects no more than four (4) DIMMS.
 # Feature request: accommodate more DIMMS.
-
-#awk_memory () { 
-
-#local ram 
-#ram=$(
-
-#awk '
-#  $1~/Memory/ && $2!~/Empty/ {Memory+=$2}
-#  END {print "Memory " Memory " GB"}
-#  ' <<< "$(system_profiler SPMemoryDataType)"
-#)
-#printf "%s\\n" "${ram}"
-#}
-
-#awk_memory 
 
 awk_memory () { 
 
@@ -40,6 +23,5 @@ printf "%s\\n" "${ram}"
 }
 
 awk_memory
-
 
 # Memory 8 GB
