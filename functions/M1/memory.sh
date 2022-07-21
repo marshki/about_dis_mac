7406aa011b8754d70929472e8453149e997c8e0a
marshki <marshall@morethantomorrow.org>
2022-07-21 14:16:39 -0400

    add

--- functions/M1/memory.sh
+++ functions/M1/memory.sh
@@ -9,19 +9,37 @@
 # Function expects no more than four (4) DIMMS.
 # Feature request: accommodate more DIMMS.
 
+#awk_memory () { 
+
+#local ram 
+#ram=$(
+
+#awk '
+#  $1~/Memory/ && $2!~/Empty/ {Memory+=$2}
+#  END {print "Memory " Memory " GB"}
+#  ' <<< "$(system_profiler SPMemoryDataType)"
+#)
+#printf "%s\\n" "${ram}"
+#}
+
+#awk_memory 
+
 awk_memory () { 
 
-local ram 
-ram=$(
+local ram
 
+ram=$(
 awk '
-  $1~/Memory/ && $2!~/Empty/ {Memory+=$2}
-  END {print "Memory " Memory " GB"}
+  $1~/Size/ && $2!~/Empty/ {size+=$2}
+  $1~/Speed/ && $2!~/Empty/ {speed=$2" "$3}
+  $1~/Type/ && $2!~/Empty/ {type=$2}
+  END {print size " GB " speed " " type}
   ' <<< "$(system_profiler SPMemoryDataType)"
 )
 printf "%s\\n" "${ram}"
 }
 
-awk_memory 
+awk_memory
+
 
 # Memory 8 GB
