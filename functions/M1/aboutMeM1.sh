bfc4bc3cbdeaf270f7396211c8ac5b9e646cfe77
marshki <marshall@morethantomorrow.org>
2022-07-27 11:44:55 -0400

    add local variables; version function

--- functions/M1/aboutMeM1.sh
+++ functions/M1/aboutMeM1.sh
@@ -19,7 +19,7 @@ RELEASE_NAME=(
 
 # Display header message.
 
-write_header () {
+write_header() {
 
   local name=$1; shift;
 
@@ -29,14 +29,16 @@ write_header () {
 
 # Parse 'short' product version and assign to variable.
 
-parse_version () {
+parse_version() {
 
   short_version=$(sw_vers -productVersion |awk -F '.' '{ print $1 }')
 }
 
 # If $short_version in table, assign macOS_name to corresponding marketing name.
 
-macOS_release_name () {
+macOS_release_name() {
+
+  local macOS_release_name
 
   if [[ -n "${RELEASE_NAME[$short_version]}" ]]; then
     macOS_release_name=${RELEASE_NAME[$short_version]}
@@ -44,20 +46,18 @@ fi
     write_header "macOS" "$macOS_release_name"
 }
 
-macOS_name_wrapper () {
-  parse_version
-  macOS_release_name
-}
+# macOS Version No.
 
 macOS_version () { 
 
+  local long_version
+
   long_version=$(sw_vers -productVersion) 
   write_header "Version" "$long_version"
-
 }
 
-
-
-
-# macOS_version
+macOS_name_wrapper() {
+  parse_version
+  macOS_release_name
+}
  
