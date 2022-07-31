4922d63e9d4b8e44f6d4370dd0835b59e21301ca
marshki <marshall@morethantomorrow.org>
2022-07-31 11:49:16 -0400

    add local variable declaration

--- functions/Intel/hardware_model_too.sh
+++ functions/Intel/hardware_model_too.sh
@@ -16,7 +16,7 @@ hardware_model () {
 
   shopt -s extglob
 
-  hardware_model=$(/usr/libexec/PlistBuddy -c "print :$(sysctl -n hw.model):_LOCALIZABLE_:marketingModel"\
+  local hardware_model=$(/usr/libexec/PlistBuddy -c "print :$(sysctl -n hw.model):_LOCALIZABLE_:marketingModel"\
   /System/Library/PrivateFrameworks/ServerInformation.framework/Versions/A/Resources/@(English|en).lproj/SIMachineAttributes.plist)
 
   printf "%s\n" "$hardware_model"
