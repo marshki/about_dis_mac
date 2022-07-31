4f289e37ebf906c4cf769a2ba37dd80d1721f22c
marshki <marshall@morethantomorrow.org>
2022-07-31 11:18:54 -0400

    repacle 'size' with 'memory'

--- functions/M1/memory.sh
+++ functions/M1/memory.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# Still WIP.
-
 # Extract RAM info.
 # Extract Memory from SPmemoryDataType output.
 # Function expects no more than four (4) DIMMS.
@@ -10,7 +8,7 @@ memory () {
 
 local ram=$(
 awk '
-  $1~/Size/ && $2!~/Empty/ {size+=$2}
+  $1~/Memory/ && $2!~/Empty/ {size+=$2}
   END {print size " GB " }
   ' <<< "$(system_profiler SPMemoryDataType)"
   )
@@ -19,7 +17,3 @@ printf "%s\\n" "${ram}"
 }
 
 memory
-
-
-# system_profiler SPMemoryDataType |awk -F ' ' /Memory:/
-# system_profiler SPMemoryDataType |awk 'FNR ==3'
