57cac26c7bd32ab946430ed1eb49ea40fcd7c752
marshki <marshall@morethantomorrow.org>
2022-07-31 11:48:26 -0400

    remove excess local variable declaration; cleanup spacing

--- functions/Intel/hardware_model.sh
+++ functions/Intel/hardware_model.sh
@@ -6,13 +6,9 @@
 # then do `sed` vodo.
 # Alternatively: sed -n 's/^.*" = "\(.*\)".*$/\1/p'
 
-# `awk` should be able to do this cleanly, too.
-
 hardware_model () {
 
-  local hardware_mod
-
-  hardware_mod=$(defaults read ~/Library/Preferences/com.apple.SystemProfiler.plist 'CPU Names' \
+  local hardware_mod=$(defaults read ~/Library/Preferences/com.apple.SystemProfiler.plist 'CPU Names' \
   | sed -E '/=/!d; s/.*= "//; s/".*//;')
 
   printf "%s\\n" "${hardware_mod}"
