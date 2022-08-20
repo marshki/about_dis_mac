23a137b8ebc7e5eb9496f4359c1e1777059adeb2
marshki <marshall@morethantomorrow.org>
2022-08-20 13:18:29 -0400

    update

--- src/aboutMe.sh
+++ src/aboutMe.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 #
-# aboutMe (Intel-based)
+# aboutMe (Intel, M1)
 #
 # CLI alternative to the macOS "About this Mac" feature.
 #
 # Author: M. Krinitz <mjk235 [at] nyu [dot] edu>
-# Date: 2021.04.22
+# Date: 2022.08.20
 # License: MIT
 
 # Lookup table.
@@ -33,9 +33,10 @@ write_header() {
   printf "%s\\n" "$@"
 }
 
-# Retrieve Apple's release name for installed operating system.
+# Retrieve Apple's "release" name for installed operating system.
+# Parse fields -> Assign macOS release number -> Assign macOS release name.
 
-parse_version() {
+parser() {
 
   IFS=. read -r field_1 field_2 < <(sw_vers -productVersion)
 }
@@ -52,92 +53,119 @@ macOS_release_number() {
 macOS_release_name() {
 
   if [[ -n "${RELEASE_NAME[$macOS_release_number]}" ]]; then
-    macOS_release_name=${RELEASE_NAME[$macOS_release_number]}
-fi
-    write_header "macOS" "$macOS_release_name"
+    macOS_name=${RELEASE_NAME[$macOS_release_number]}
+  fi
+
+    write_header "macOS" "$macOS_name"
 }
 
-macOS_release_name_wrapper() {
+# Release name wrapper.
 
-  parse_version
+release_name_wrapper() {
+
+  parser
   macOS_release_number
   macOS_release_name
 }
 
-# Retrieve operating system version.
+# Version.
 
 macOS_version() {
 
-  local os
+  local long_version
 
-  os=$(sw_vers -productVersion)
+  long_version=$(sw_vers -productVersion)
 
-  write_header "Version" "$os"
+  write_header "Version" "$long_version"
 }
 
-# Retrieve hardware model.
+# Hardware_model.
 
 hardware_model() {
- 
+
   local hardware_mod
 
   hardware_mod=$(defaults read ~/Library/Preferences/com.apple.SystemProfiler.plist 'CPU Names' \
     | sed -E '/=/!d; s/.*= "//; s/".*//;')
-    
+
   write_header "Hardware Model" "$hardware_mod"
 }
 
-# Retrieve processor information.
+# Processor (Intel, M1)
 
-processor() {
+Intel_processor() {
 
   local cpu
- 
+
   cpu=$(system_profiler SPHardwareDataType \
-    | awk '/Processor (Name|Speed):/ { sub(/^.*: /, ""); print; }'\
-    | sort \
-    | xargs)
+  | awk '/Processor (Name|Speed):/ { sub(/^.*: /, ""); print; }'\
+  | sort \
+  | xargs)
 
-  write_header "Processor" "$cpu"
+  "write_header" "Processor" "$cpu"
 }
 
-# Retrieve memory information.
+# Processor (M1).
 
-memory() {
+M1_processor() {
 
-  local ram
+  local cpu
 
-  ram=$(
-  awk '
-    $1~/Size/ && $2!~/Empty/ {size+=$2}
-    $1~/Speed/ && $2!~/Empty/ {speed=$2" "$3}
-    $1~/Type/ && $2!~/Empty/ {type=$2}
-    END {print size " GB " speed " " type}
-    ' <<< "$(system_profiler SPHardwareDataType; system_profiler SPMemoryDataType)"
+  cpu=$(system_profiler SPHardwareDataType \
+    | awk '/Chip:/{ sub(/^.*: /, ""); print; }')
+
+  "write_header" "Processor" "$cpu"
+}
+
+# Memory (Intel, M1)
+
+Intel_memory() {
+
+local ram
+
+ram=$(
+
+awk '
+  $1~/Size/ && $2!~/Empty/ {size+=$2}
+  $1~/Speed/ && $2!~/Empty/ {speed=$2" "$3}
+  $1~/Type/ && $2!~/Empty/ {type=$2}
+  END {print size " GB " speed " " type}
+  ' <<< "$(system_profiler SPMemoryDataType)"
 )
-
-  write_header "Memory" "${ram}"
+  write_header "Memory" "$ram"
 }
 
-# Retrieve startup disk info.
+M1_memory() {
+  local ram 
+
+ram=$(
+awk '
+  $1~/Memory/ && $2!~/Empty/ {size+=$2}
+  END {print size " GB " }
+  ' <<< "$(system_profiler SPMemoryDataType)"
+  )
+
+  write_header "Memory" "$ram"
+}
+
+# Startup disk.
 
 startup_disk() {
 
   local disk
-  
+
   disk=$(system_profiler SPStorageDataType \
-    | awk 'FNR == 3 {print}'\
-    | sed 's/[[:blank:]:]*//g')
+   |awk 'FNR == 3 {print}' |sed 's/[[:blank:]:]*//g')
   
   write_header "Startup Disk" "$disk"
 }
 
-# Retrieve graphics info.
+# Graphics.
 
 graphics() {
 
-  local gpu
-  
+  local gpu    
+
   gpu=$(system_profiler SPDisplaysDataType 2>/dev/null \
     | awk '/(Model|Max\)|Total\)):/ { sub(/^.*: /, ""); print; }' \
     | xargs)
@@ -145,30 +173,56 @@ graphics() {
   write_header "Graphics" "$gpu"
 }
 
-# Retrieve serial number.
+# Serial number.
 
 serial_number() {
 
   local serialnum
 
-  serialnum=$(system_profiler SPHardwareDataType \
-    | awk '/Serial/ { sub(/^.*: /, ""); print; }')
+  serialnum=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
 
   write_header "Serial Number" "$serialnum"
+}
+
+# Intel wrapper.
+
+Intel_wrapper() {
+  
+  release_name_wrapper
+  macOS_version
+  hardware_model
+  Intel_processor
+  Intel_memory
+  startup_disk
+  graphics
+  serial_number
+}
+
+# M1 wrapper.
+
+M1_wrapper() {
+  release_name_wrapper
+  macOS_version
+  hardware_model
+  M1_processor
+  M1_memory
+  startup_disk
+  serial_number
 }
 
 # Las entranas del programa.
 
 main() {
 
-	macOS_release_name_wrapper
-	macOS_version
-	hardware_model
-	processor
-	memory
-	startup_disk
-	graphics
-	serial_number
+  if [[ $(uname -m) == 'arm64' ]]; then
+    M1_wrapper
+
+  elif [[ $(uname -m) == 'x86_64' ]]; then
+    Intel_wrapper
+
+  else
+    printf "%s\n" "Unable to detect system architecture. Exiting."
+fi
 }
 
 main "$@"
