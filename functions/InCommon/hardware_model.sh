b15fded457dc18292c70485e1e10d5eaffad7270
marshki <marshall@morethantomorrow.org>
2022-11-08 12:06:28 -0500

    add URL of interest

--- functions/InCommon/hardware_model.sh
+++ functions/InCommon/hardware_model.sh
@@ -7,7 +7,8 @@
 
 # Preference list did not appear in some Apple Silicon-based machines
 # may need to look here: system_profiler SPHardwareDataType as an alternative. 
-
+# Of possible interest: 
+# https://apple.stackexchange.com/questions/98080/can-a-macs-model-year-be-determined-with-a-terminal-command
 
 hardware_model () {
 
