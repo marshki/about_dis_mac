b75e3233d2864417375d7414039c304ea1b94b20
marshki <marshall@morethantomorrow.org>
2022-11-08 16:23:20 -0500

    rename dir

---
+++ functions/AppleSilicon/memory.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Extract RAM info.
+# Extract Memory from SPmemoryDataType output.
+# Function expects no more than four (4) DIMMS.
+
+memory () {
+  local ram 
+
+ram=$(
+awk '
+  $1~/Memory/ && $2!~/Empty/ {size+=$2}
+  END {print size " GB " }
+  ' <<< "$(system_profiler SPMemoryDataType)"
+  )
+
+  printf "%s\\n" "${ram}"
+}
+
+memory
