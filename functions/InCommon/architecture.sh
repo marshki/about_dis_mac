debbdea46974bc66b9b25ed98d9950ac48c9636c
marshki <marshall@morethantomorrow.org>
2022-11-09 10:44:17 -0500

    M1 to Apple Silicon

--- functions/InCommon/architecture.sh
+++ functions/InCommon/architecture.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash 
 
-# Detect system architecture (x86_64, M1).
+# Detect system architecture (x86_64, Apple Silicon).
 
 architecture() { 
 
   if [[ $(uname -m) == 'arm64' ]]; then
-    printf "%s\n" "M1 architecture"
+    printf "%s\n" "Apple Silicon architecture"
 
   elif [[ $(uname -m) == 'x86_64' ]]; then
     printf "%s\n" "Intel architecture"
